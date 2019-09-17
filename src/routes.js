814acf622e6d0034172c5f4f364d004885a50670
Dev-Rafael_Vieira <rafael_egqsociesc@hotmail.com>
2019-09-17 02:03:45 -0300

    Primeiro layout do App

---
+++ src/routes.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Image } from 'react-native';
+import { createAppContainer} from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';//`createStackNavigator()` has been moved to `react-navigation-stack`
+
+import logo from './assets/instagram.png'
+
+import Feed from './pages/Feed';
+
+const Routes = createAppContainer(
+    createStackNavigator({
+        Feed
+    }, {
+        headerLayoutPreset: 'center',
+        defaultNavigationOptions: {
+            headerTitle: <Image source={logo} />
+        },
+    })
+);
+
+export default Routes;
